6200470cd36704255ff7f010a99a371234f054bc
jez <jezreeljoshuam@gmail.com>
2023-12-12 23:02:49 +0800

    more plugins

--- init.lua
+++ init.lua
@@ -67,15 +67,75 @@ end
 vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
 	{
-	'nvim-telescope/telescope.nvim', tag='0.1.5',
-	dependencies = { 'nvim-lua/plenary.nvim' }
-	}
+		'nvim-telescope/telescope.nvim', tag='0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{   
+		'olivercederborg/poimandres.nvim',
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require('poimandres').setup {
+				-- leave this setup function empty for default config
+				-- or refer to the configuration section
+				-- for configuration options
+			}
+		end,
+
+		-- optionally set the colorscheme within lazy config
+		init = function()
+			vim.cmd("colorscheme poimandres")
+		end
+	},
+	{
+		"nvim-treesitter/nvim-treesitter", 
+		build = ":TSUpdate",
+		config = function () 
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				ensure_installed = {"help", "lua", "vim", "vimdoc", "javascript", "typescript", "html", "go" },
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },  
+			})
+		end
+	},
+	{
+		"ThePrimeagen/harpoon",
+	},
+	{
+		'mbbill/undotree'
+	},
+	{
+		'tpope/vim-fugitive'
+	},
 }, {})
 
-
+-- Telescope config
 local builtin = require('telescope/builtin')
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>gs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+
+-- Harpoon config
+local mark = require('harpoon.mark')
+local ui = require('harpoon.ui')
+
+vim.keymap.set('n', '<leader>a', mark.add_file)
+vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+
+vim.keymap.set('n', '<C-h>', function() ui.nav_file(1) end)
+vim.keymap.set('n', '<C-j>', function() ui.nav_file(2) end)
+vim.keymap.set('n', '<C-k>', function() ui.nav_file(3) end)
+vim.keymap.set('n', '<C-l>', function() ui.nav_file(4) end)
+
+
+-- Undo tree config
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+
+-- Fugitive config
+vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
