36d3779e25b0f9966a583db4c9721fea6b56416c
jez <jezreeljoshuam@gmail.com>
2023-12-25 15:58:05 +0800

    more configs

--- init.lua
+++ init.lua
@@ -79,8 +79,7 @@ require('lazy').setup({
         dependencies = { 'nvim-tree/nvim-web-devicons' }
     },
 
-    { 'jiangmiao/auto-pairs' }
-
+    { 'jiangmiao/auto-pairs' },
 }, {})
 
 -- Telescope config
@@ -135,6 +134,7 @@ require('lualine').setup({
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
+lsp.nvim_workspace()
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
@@ -168,13 +168,47 @@ end)
 
 lsp.setup()
 
+
+local autocmd = vim.api.nvim_create_autocmd
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = {},
+    ensure_installed = {'gopls'},
     handlers = {
         lsp.default_setup,
+        gopls = function ()
+            require('lspconfig').gopls.setup({
+                autocmd("BufWritePre", {
+                    pattern = "*.go",
+                    callback = function()
+                        local params = vim.lsp.util.make_range_params()
+                        params.context = {only = {"source.organizeImports"}}
+                        -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+                        -- machine and codebase, you may want longer. Add an additional
+                        -- argument after params if you find that you have to write the file
+                        -- twice for changes to be saved.
+                        -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+                        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+                        for cid, res in pairs(result or {}) do
+                            for _, r in pairs(res.result or {}) do
+                                if r.edit then
+                                    local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                                    vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                                end
+                            end
+                        end
+                        vim.lsp.buf.format({async = false})
+                    end
+                }),
+                completeUnimported = true,
+                usePlaceholders = true,
+                analyses = {
+                    unusedparams = true
+                }
+            })
+        end
     },
 })
+
 
 
 -- Primeagen editor config  might want to remove some of these
@@ -230,8 +264,8 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<C-m>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-n>", "<cmd>cprev<CR>zz")
+-- vim.keymap.set("n", "<C-m>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-n>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
