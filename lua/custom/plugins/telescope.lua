5fdf98063cfb7446341dc46b16a0ca6d2fb52306
jez <jezreeljoshuam@gmail.com>
2024-01-01 21:40:53 +0800

    update config

---
+++ lua/custom/plugins/telescope.lua
@@ -0,0 +1,26 @@
+
+return {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local builtin = require('telescope/builtin')
+        vim.keymap.set('n', '<leader>af', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+        --- Install ripgrep for this to work
+        vim.keymap.set('n', '<leader>gr', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") });
+        end)
+
+        require('telescope').setup({
+            pickers = {
+                find_files = {
+                    disable_devicons = true
+                },
+                git_files = {
+                    disable_devicons = true
+                }
+            }
+        })
+    end
+}
---
+++ lua/custom/remap.lua
@@ -0,0 +1,37 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>q', vim.cmd.q)
+vim.keymap.set('n', '<leader>w', '<C-w>')
+
+-- Primeagen remaps: consider removing some of them or at least edit them. might work for now tho
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>vwm", function()
+    require("vim-with-me").StartVimWithMe()
+end)
+vim.keymap.set("n", "<leader>svwm", function()
+    require("vim-with-me").StopVimWithMe()
+end)
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ 'n', 'v' }, '<leader>p', [["+p]])
+vim.keymap.set('n', '<leader>P', [["+P]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format)
+
+-- vim.keymap.set("n", "<C-m>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-n>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
