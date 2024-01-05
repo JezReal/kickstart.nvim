93bdb67ad0a0ab88a183c7ddbadff3abf0bb5aa8
jez <jezreeljoshuam@gmail.com>
2024-01-06 01:29:08 +0800

    setup lsp (again)

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -1,79 +1,66 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "gopls", "tsserver", "html", "emmet_language_server" },
-			})
-		end,
-	},
-	-- i dont think nvimm-cmp belongs here. should maybe move to `completion.lua`
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"L3MON4D3/LuaSnip",
-		},
-		config = function()
-			local cmp = require("cmp")
-			local cmp_select = { behavior = cmp.SelectBehavior.Select }
-			cmp.setup({
-				mapping = cmp.mapping.preset.insert({
-					["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-					["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<C-Space>"] = cmp.mapping.complete(),
-				}),
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{ "hrsh7th/cmp-nvim-lsp" },
-		},
-		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.gopls.setup({
-				settings = {
-					gopls = {
-						analyses = {
-							unusedparams = true,
-						},
-						staticcheck = true,
-						gofumpt = true,
-					},
-				},
-			})
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "gopls",
+                    "tsserver",
+                    "html",
+                    "emmet_language_server",
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.gopls.setup({
+                settings = {
+                    gopls = {
+                        analyses = {
+                            unusedparams = true,
+                        },
+                        staticcheck = true,
+                        gofumpt = true,
+                    },
+                },
+            })
+            lspconfig.tsserver.setup({})
+            lspconfig.html.setup({})
 
-			-- lsp keymaps
-			vim.keymap.set("n", "gd", function()
-				vim.lsp.buf.definition()
-			end, opts)
-			vim.keymap.set("n", "K", function()
-				vim.lsp.buf.hover()
-			end, opts)
-			vim.keymap.set("n", "<leader>ws", function()
-				vim.lsp.buf.workspace_symbol()
-			end, opts)
-			vim.keymap.set("n", "<leader>ca", function()
-				vim.lsp.buf.code_action()
-			end, opts)
-			vim.keymap.set("n", "<leader>cr", function()
-				vim.lsp.buf.references()
-			end, opts)
-			vim.keymap.set("n", "<leader>rn", function()
-				vim.lsp.buf.rename()
-			end, opts)
-			vim.keymap.set("i", "<C-h>", function()
-				vim.lsp.buf.signature_help()
-			end, opts)
-		end,
-	},
+
+            -- keymaps
+            vim.keymap.set("n", "gd", function()
+                vim.lsp.buf.definition()
+            end, opts)
+            vim.keymap.set("n", "K", function()
+                vim.lsp.buf.hover()
+            end, opts)
+            vim.keymap.set("n", "<leader>ws", function()
+                vim.lsp.buf.workspace_symbol()
+            end, opts)
+            vim.keymap.set("n", "<leader>ca", function()
+                vim.lsp.buf.code_action()
+            end, opts)
+            vim.keymap.set("n", "<leader>cr", function()
+                vim.lsp.buf.references()
+            end, opts)
+            vim.keymap.set("n", "<leader>rn", function()
+                vim.lsp.buf.rename()
+            end, opts)
+            vim.keymap.set("i", "<C-h>", function()
+                vim.lsp.buf.signature_help()
+            end, opts)
+        end,
+    },
 }
