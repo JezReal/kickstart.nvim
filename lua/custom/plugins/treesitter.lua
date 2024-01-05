cb9d69cf2d3551957e8cb532d355b50e4ffef4c2
jez <jezreeljoshuam@gmail.com>
2024-01-05 22:50:46 +0800

    auto install parsers

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -14,6 +14,7 @@ return {
                 "html",
                 "go"
             },
+            auto_install = true,
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },
