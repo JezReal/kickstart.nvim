5e4d5d52a06ef4643f7b7f77826ac355605bf7b7
jez <jezreeljoshuam@gmail.com>
2024-04-26 00:10:13 +0800

    show color column properly

--- lua/custom/plugins/color_theme.lua
+++ lua/custom/plugins/color_theme.lua
@@ -4,12 +4,13 @@ return {
     priority = 1000,
     config = function()
         require("tokyonight").setup({
-            transparent = false
+            transparent = false,
         })
     end,
 
     -- optionally set the colorscheme within lazy config
     init = function()
         vim.cmd("colorscheme tokyonight-moon")
+        vim.cmd("hi ColorColumn ctermbg=lightgrey guibg=lightgrey")
     end,
 }
