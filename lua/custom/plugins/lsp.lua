9e797de1ccdf39dc0b42ef08b93b54b9b4c964d1
jez <jezreeljoshuam@gmail.com>
2024-05-19 22:44:40 +0800

    add vue support

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -62,7 +62,20 @@ return {
             })
 
             lspconfig.pyright.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
+            })
+
+            lspconfig.volar.setup({
+                capabilities = capabilities,
+                filetypes = { "vue", "javascript", "typescript", "javascriptreact", "typescriptreact" },
+                init_options = {
+                    vue = {
+                        hybridMode = false,
+                    },
+                    typescript = {
+                        tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+                    },
+                },
             })
 
             -- keymaps
