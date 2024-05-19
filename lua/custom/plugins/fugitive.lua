7f766360b0496983de8163fe5d7c2d2561cff811
jez <jezreeljoshuam@gmail.com>
2024-05-19 22:30:52 +0800

    config

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -3,7 +3,7 @@ return {
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
         vim.keymap.set("n", "<leader>gp", function()
-            vim.cmd("G push")
+            vim.cmd("Git -c push.default=current push")
         end)
         vim.keymap.set("n", "<leader>gu", function()
             vim.cmd("G pull")
