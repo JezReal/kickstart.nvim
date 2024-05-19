c627769ee664d9c1afbff3ff511a85202ef50eb9
jez <jezreeljoshuam@gmail.com>
2024-05-19 23:53:07 +0800

    config

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -1,118 +1,118 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "gopls",
-                    "tsserver",
-                    "html",
-                    "emmet_language_server",
-                    "templ",
-                    "volar",
-                    "svelte",
-                    "astro"
-                },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"gopls",
+					"tsserver",
+					"html",
+					"emmet_language_server",
+					"templ",
+					"volar",
+					"svelte",
+					"astro",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.gopls.setup({
-                capabilities = capabilities,
-                settings = {
-                    gopls = {
-                        analyses = {
-                            unusedparams = true,
-                        },
-                        usePlaceholders = true,
-                        completeUnimported = true,
-                        staticcheck = true,
-                        gofumpt = true,
-                    },
-                },
-                filetypes = { "go", "gomod", "gowork", "gotmpl", "templ" }
-            })
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+				settings = {
+					gopls = {
+						analyses = {
+							unusedparams = true,
+						},
+						usePlaceholders = true,
+						completeUnimported = true,
+						staticcheck = true,
+						gofumpt = true,
+					},
+				},
+				filetypes = { "go", "gomod", "gowork", "gotmpl", "templ" },
+			})
 
-            lspconfig.tsserver.setup({
-                capabilities = capabilities,
-                init_options = {
-                    preferences = {
-                        disableSuggestions = true,
-                    },
-                },
-            })
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+				init_options = {
+					preferences = {
+						disableSuggestions = true,
+					},
+				},
+			})
 
-            lspconfig.html.setup({
-                capabilities = capabilities,
-                filetypes = { "html", "templ", "htmldjango", "vue" },
-            })
-            lspconfig.templ.setup({
-                vim.filetype.add({ extension = { templ = "templ" } }),
-                capabilities = capabilities,
-            })
+			lspconfig.html.setup({
+				capabilities = capabilities,
+				filetypes = { "html", "templ", "htmldjango" },
+			})
+			lspconfig.templ.setup({
+				vim.filetype.add({ extension = { templ = "templ" } }),
+				capabilities = capabilities,
+			})
 
-            lspconfig.pyright.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
 
-            lspconfig.volar.setup({
-                capabilities = capabilities,
-                filetypes = { "vue", "javascript", "typescript", "javascriptreact", "typescriptreact" },
-                init_options = {
-                    vue = {
-                        hybridMode = false,
-                    },
-                    typescript = {
-                        tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
-                    },
-                },
-            })
+			lspconfig.volar.setup({
+				capabilities = capabilities,
+				filetypes = { "vue", "javascript", "typescript", "javascriptreact", "typescriptreact" },
+				init_options = {
+					vue = {
+						hybridMode = false,
+					},
+					typescript = {
+						tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+					},
+				},
+			})
 
-            lspconfig.svelte.setup({
-                capabilities = capabilities
-            })
+			lspconfig.svelte.setup({
+				capabilities = capabilities,
+			})
 
-            lspconfig.astro.setup({
-                capabilities = capabilities
-            })
+			lspconfig.astro.setup({
+				capabilities = capabilities,
+			})
 
-            -- keymaps
-            vim.keymap.set("n", "gd", function()
-                vim.lsp.buf.definition()
-            end, opts)
-            vim.keymap.set("n", "K", function()
-                vim.lsp.buf.hover()
-            end, opts)
-            vim.keymap.set("n", "<leader>ws", function()
-                vim.lsp.buf.workspace_symbol()
-            end, opts)
-            vim.keymap.set("n", "<leader>ca", function()
-                vim.lsp.buf.code_action()
-            end, opts)
-            vim.keymap.set("n", "<leader>cr", function()
-                vim.lsp.buf.references()
-            end, opts)
-            vim.keymap.set("n", "<leader>rn", function()
-                vim.lsp.buf.rename()
-            end, opts)
-            vim.keymap.set("i", "<C-h>", function()
-                vim.lsp.buf.signature_help()
-            end, opts)
-        end,
-    },
+			-- keymaps
+			vim.keymap.set("n", "gd", function()
+				vim.lsp.buf.definition()
+			end, opts)
+			vim.keymap.set("n", "K", function()
+				vim.lsp.buf.hover()
+			end, opts)
+			vim.keymap.set("n", "<leader>ws", function()
+				vim.lsp.buf.workspace_symbol()
+			end, opts)
+			vim.keymap.set("n", "<leader>ca", function()
+				vim.lsp.buf.code_action()
+			end, opts)
+			vim.keymap.set("n", "<leader>cr", function()
+				vim.lsp.buf.references()
+			end, opts)
+			vim.keymap.set("n", "<leader>rn", function()
+				vim.lsp.buf.rename()
+			end, opts)
+			vim.keymap.set("i", "<C-h>", function()
+				vim.lsp.buf.signature_help()
+			end, opts)
+		end,
+	},
 }
